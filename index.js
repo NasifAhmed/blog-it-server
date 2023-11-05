a75c857a482f86a4c010ad0374d6d4bfe61997d4
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-11-05 13:35:05 +0600

    Setup mongodb and add dotenv package

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Imports
 const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
 // Setup
 const app = express();
@@ -7,6 +9,27 @@ const port = 4000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+// MongoDB Setup
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nmxrrle.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+async function run() {
+    try {
+        // Connect the client to the server (optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!"
+        );
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+const database = client.db("book-collection");
+const userCollection = database.collection("users");
+const bookCollection = database.collection("books");
 
 // Middlewares
 app.use(express.json());
@@ -18,6 +41,6 @@ function logger(req, res, next) {
 }
 
 // Routes
-app.get("/", logger, (req, res) => {
-    res.send("Server is running.......");
+app.get("/", logger, async (req, res) => {
+    res.send(`Server is running.......`);
 });
